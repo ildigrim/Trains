f5e6e09aff7b5e195ad8996f779b981037e19907
Eugene Guz <ildigrim.ua@gmail.com>
2022-09-17 17:22:38 +0300

    Adding class for game logic.

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,3 @@
 public class Game {
-    public static void main(String[] args) {
-        CardDeck.CreateDeck();
-        CardDeck.deckDisplay();
-        CardDeck.deckShuffle();
-        System.out.println();
-        CardDeck.deckDisplay();
-    }
+
 }
