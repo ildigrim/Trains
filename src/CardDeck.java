9b23556f9c8fb06959128b0dc0d73a6fb0884dbd
Eugene Guz <ildigrim.ua@gmail.com>
2022-09-17 17:33:05 +0300

    Enum Card in separate file.

--- src/CardDeck.java
+++ src/CardDeck.java
@@ -2,38 +2,50 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-enum Card {pinc, white, blue, yellow, orange, black, red, green, locomotive}
+
 
 public class CardDeck {
-    public static List <Card> deck = new ArrayList<>();
+    public List <Card> deck = new ArrayList<>();
 
-    public static void CreateDeck (){
-        for (int i = 0; i<12; i++) {
-            addCard(Card.pinc);
-            addCard(Card.white);
-            addCard(Card.blue);
-            addCard(Card.yellow);
-            addCard(Card.orange);
-            addCard(Card.black);
-            addCard(Card.red);
-            addCard(Card.green);
-            addCard(Card.locomotive);
-        }
-        addCard(Card.locomotive);
-        addCard(Card.locomotive);
+    CardDeck (){
+        createDeck();
+        deckShuffle();
     }
 
-    static void addCard(Card card){
+    public void createDeck (){
+        for (int i = 0; i<12; i++) {
+            addCard(Card.PINC);
+            addCard(Card.WHITE);
+            addCard(Card.BLUE);
+            addCard(Card.YELLOW);
+            addCard(Card.ORANGE);
+            addCard(Card.BLACK);
+            addCard(Card.RED);
+            addCard(Card.GREEN);
+            addCard(Card.LOCOMOTIVE);
+        }
+        addCard(Card.LOCOMOTIVE);
+        addCard(Card.LOCOMOTIVE);
+    }
+
+    private void addCard(Card card){
         deck.add(card);
     }
 
-    static void deckDisplay(){
+    public void deckDisplay(){
         for (Card card: deck){
             System.out.println(card);
         }
     }
 
-    static void deckShuffle(){
+    public void deckShuffle(){
         Collections.shuffle(deck);
+    }
+
+    void cardsOnTable(){
+
+    }
+    void cardsToPlayer(){
+
     }
 }
