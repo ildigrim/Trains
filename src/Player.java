a57c092c0eec6a5c005bf0463a5ea64c75c82cad
Eugene Guz <ildigrim.ua@gmail.com>
2022-09-20 22:40:01 +0300

    Extending player's data storage.

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
+
+    private final List<Card> cards;
+    private final String name;
+    private final PlayersColors color;
+
+    Player(List<Card> cards, String name, PlayersColors color){
+        this.cards = cards;
+        this.name = name;
+        this.color = color;
+    }
+
+    public PlayersColors getColor() {
+        return color;
+    }
+
 }
