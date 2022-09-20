9b3d0b88da62f336c682850c5646c2f8e7b0fedd
Eugene Guz <ildigrim.ua@gmail.com>
2022-09-20 22:42:31 +0300

    Creating discard.

---
+++ src/Discard.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Discard {
+
+    private final List<Card> cards = new ArrayList<>();
+
+    public void addCard(Card card){
+        cards.add(card);
+    }
+
+
+}
