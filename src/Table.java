69136a738e2de3915e3094eaa51f4f6ac094c436
Eugene Guz <ildigrim.ua@gmail.com>
2022-09-20 22:40:30 +0300

    Extending player's data storage.

---
+++ src/Table.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Table {
+
+    private final List<Card> cards = new ArrayList<>();
+
+    public List<Card> getCards (){return cards;}
+
+    public void addCard(Card card){
+        cards.add(card);
+    }
+
+    public boolean hasThreeLocomotives(){
+        int i = 0;
+        for(Card card: cards){
+            if (card.equals(Card.LOCOMOTIVE)){
+                i++;
+            }
+        }
+        return i>2;
+    }
+
+    public Card getCard(int index){
+        Card card = cards.get(index);
+        cards.remove(index);
+        return card;
+    }
+
+}
